9bf527fe18574c6925189e951f1f6e4d21ae8b82
monarie <jobmonari7@gmail.com>
2023-05-09 03:46:21 +0300

    changing spelling mistakes

--- src/index.ts
+++ src/index.ts
@@ -11,5 +11,5 @@ app.get('/', (req: Request, res: Response) => {
 
 
 app.listen(3000, () => {
-    console.log(`app is listening on posrt 3000`)
+    console.log(`app is listening on port 3000`)
 })
